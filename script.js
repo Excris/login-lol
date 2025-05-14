cff9f966d123428af410bffd53da95641e2e186d
Excris <cristhianerik.2019@gmail.com>
2025-05-13 23:39:33 -0300

    ajuste Imagem

--- script.js
+++ script.js
@@ -13,5 +13,16 @@ const handleFocusOut = ({target}) => {
     
  }
 
+ const handleChange = () => {
+    const [username, password] = inputs;
+
+    if (username.value && password.value.length >= 8){
+        button.removeAttribute('disabled');
+    }else{
+        button.setAttribute('disabled', '');
+    }
+ };
+
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+inputs.forEach((input) => input.addEventListener('input', handleChange));
